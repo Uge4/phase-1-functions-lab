e5b2370e7e6adc2b3b222a39c5ca6874f1de77d1
eugene <eugeneholenstein@gmail.com>
2022-11-22 23:51:56 +1100

    commit tree 307c7ba2fe2bd24e3070b31cbf4b9021a88ea6cc

--- index.js
+++ index.js
@@ -1,2 +1,44 @@
+
 // Code your solution in this file!
-distanceFromHqInBlocks()
+
+function distanceFromHqInBlocks(someValue){
+    return Math.abs(someValue - 42);
+    // console.log(Math.abs(someValue - 42));
+}
+distanceFromHqInBlocks();
+
+
+function distanceFromHqInFeet(someValue){
+    return distanceFromHqInBlocks(someValue) * 264;
+    // console.log(distanceFromHqInBlocks(someValue) * 264);
+
+}
+distanceFromHqInFeet();
+
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs(destination-start) * 264;
+    // console.log(Math.abs(destination-start) * 264);
+}
+distanceTravelledInFeet()
+
+
+function calculatesFarePrice(start, destination){
+
+    const travelled = distanceTravelledInFeet(start, destination)
+
+        if(travelled < 400){
+            return 0
+        }
+        else if(travelled > 400 && travelled < 2000){
+            return (travelled - 400) * 0.02
+        }
+        else if(travelled > 2000 && travelled < 2500){
+            return 25
+        }
+        else{
+            return 'cannot travel that far'
+        }
+}
+calculatesFarePrice(50,58)
+
